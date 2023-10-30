77f770ddccd1e95dcf6d0c07d9e67219e61ef9e6
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-10-30 11:54:44 +0800

    118

--- leetcode/118.go
+++ leetcode/118.go
@@ -1,18 +1,25 @@
 package leetcode
 
+/*
+*
+1
+11
+121
+1331
+*/
 func generate(numRows int) [][]int {
-	//给二维的定义长度
-	ans := make([][]int, numRows)
+	//给二维数组分配内存
+	res := make([][]int, numRows)
 	for i := 0; i < numRows; i++ {
-		//给一维的定义长度
-		ans[i] = make([]int, i+1)
-		//首末位置都为1
-		ans[i][0] = 1
-		ans[i][i] = 1
-		//其他位置单独处理
+		//给一维数组分配内存，注意i是从0开始的，所以要+1
+		res[i] = make([]int, i+1)
+		//首先给首末
+		res[i][0] = 1
+		res[i][i] = 1
+		//其他位置就是用该位置的上一个位置和上一个位置前一个位置进行得出来的
 		for j := 1; j < i; j++ {
-			ans[i][j] = ans[i-1][j] + ans[i-1][j-1]
+			res[i][j] = res[i-1][j] + res[i-1][j-1]
 		}
 	}
-	return ans
+	return res
 }
