878a4e7f8039716cf2b581bfe9cd3ea67f285fa8
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-11-04 12:58:19 +0800

    148

--- leetcodeHot100/148.go
+++ leetcodeHot100/148.go
@@ -1,32 +1,43 @@
 package leetcodeHot100
 
 func sortList(head *ListNode) *ListNode {
+	//特殊情况进行处理
 	if head == nil || head.Next == nil {
 		return head
 	}
-
-	dummy := &ListNode{Val: 0, Next: head}
-	n := 0
-	for node := head; node != nil; node = node.Next {
-		n++
+	//为了找到中间位置，定义快慢指针
+	left := head
+	right := head.Next
+	for right != nil && right.Next != nil {
+		//慢指针走一步
+		left = left.Next
+		//快指针走两步
+		right = right.Next.Next
 	}
-
-	for i := 0; i < n-1; i++ {
-		// 使用两个指针prev和curr来遍历链表并比较相邻节点的值
-		prev := dummy
-		curr := dummy.Next
-		for j := 0; j < n-i-1; j++ {
-			if curr.Val > curr.Next.Val {
-				// 交换相邻节点的值
-				temp := curr.Next
-				curr.Next = curr.Next.Next
-				temp.Next = curr
-				prev.Next = temp
-			}
-			prev = curr
-			curr = curr.Next
+	//当走到这里的时候，left刚好指向中间位置
+	//记录后半部分的指针
+	mid := left.Next
+	//断开前半部分跟后半部分的连接
+	left.Next = nil
+	//递归，注意返回的是排序后的头节点
+	l := sortList(head)·
+	r := sortList(mid)
+	res := &ListNode{Val: 0}
+	ans := res
+	for l != nil && r != nil {
+		if l.Val < r.Val {
+			res.Next = l
+			l = l.Next
+		} else {
+			res.Next = r
+			r = r.Next
 		}
+		res = res.Next
 	}
-
-	return dummy.Next
+	if l == nil {
+		res.Next = r
+	} else {
+		res.Next = l
+	}
+	return ans.Next
 }
