8678f3d0fc4b867931ec771f0826a6c379f4e6e2
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-11-08 22:57:40 +0800

    136

---
+++ leetcodeHot100/136.go
@@ -0,0 +1,13 @@
+package leetcodeHot100
+
+// 这道题的核心思想是:
+// 两个相同的值异或值为0 比如 1^1=0
+// 一个0和一个值相异或 得到的是这个值 1^0=1
+// 所以就可以得出来只出现一次的数字了
+func singleNumber(nums []int) int {
+	var res = 0
+	for i := 0; i < len(nums); i++ {
+		res ^= nums[i]
+	}
+	return res
+}
