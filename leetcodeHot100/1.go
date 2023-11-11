5f3dc8017de0561f10fb37b11f65299b0422dba1
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-11-11 23:24:26 +0800

    1

---
+++ leetcodeHot100/1.go
@@ -0,0 +1,13 @@
+package leetcodeHot100
+
+func twoSum(nums []int, target int) []int {
+	//暴力解法
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
