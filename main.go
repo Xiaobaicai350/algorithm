6d1205ef3a5d554768dc8562799d53d8078db616
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-11-12 22:40:49 +0800

    94

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	res := make([]int, 1)
+	demo(res)
+	fmt.Printf("%d", res)
+}
+func demo(res []int) {
+	res[0] = 1
+	res = append(res, 2)
 }
