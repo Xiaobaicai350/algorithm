aa6cf385dfddfcd550820d13080d31213091068a
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-11-12 21:58:03 +0800

    17

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"algorithm/leetcodeHot100"
-	"fmt"
-)
-
 func main() {
-	ints := []int{1, 2, 3}
-	permute := leetcodeHot100.Permute(ints)
-	for i, temp := range permute {
-		fmt.Println(i, "  ", temp)
-	}
+
 }
