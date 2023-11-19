a4171ff5d60fd34407710222056e948d09f2a69a
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-11-19 22:43:55 +0800

    114

--- leetcodeHot100/114.go
+++ leetcodeHot100/114.go
@@ -1,6 +1,12 @@
 package leetcodeHot100
 
 // https://bilibili.com/video/BV14s4y1U77K/?spm_id_from=333.337.search-card.all.click&vd_source=2259e5459a8cfd21bcf92bc46bf3beda
+// 该算法的步骤是：
+// 1.当前节点不为nil的时候，就往下遍历
+// 2.如果左节点为空，就往下遍历，找下一个左节点不为空的节点
+// 3.当找到左节点不为空的时候，就找当前节点的左子树的最后一个右子树（这里是为了拼接当前节点的右子树）
+// 4.之后就是拼接，然后把当前节点的左子树转换成右子树
+// 5.把左子树置空，开始遍历下一个右子树，进行循环，直到为空
 func flatten(root *TreeNode) {
 	for root != nil {
 		//如果左子树为nil，就直接遍历下一个右子树
