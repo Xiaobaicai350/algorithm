862d987ab367804a770a56050d9c91b3a080597e
tianhaoxu_i <tianhaoxu_i@didiglobal.com>
2023-11-19 20:46:58 +0800

    230

--- leetcodeHot100/230.go
+++ leetcodeHot100/230.go
@@ -1,6 +1,17 @@
 package leetcodeHot100
 
+var ints []int
+
 func kthSmallest(root *TreeNode, k int) int {
-	vec := traversal(root)
-	return vec[k]
+	ints = make([]int, 0)
+	inorderKth(root)
+	return ints[k]
+}
+func inorderKth(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	inorderKth(root.Left)
+	ints = append(ints, root.Val)
+	inorderKth(root.Right)
 }
