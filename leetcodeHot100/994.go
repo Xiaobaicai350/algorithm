54237a2dad7e7e6c4e04489742de431a321fda03
胥天昊 <1499487526@qq.com>
2024-03-10 13:02:52 +0800

    994

--- leetcodeHot100/994.go
+++ leetcodeHot100/994.go
@@ -1,37 +1,83 @@
 package leetcodeHot100
 
+/*
+*
+值 0 代表空单元格；
+值 1 代表新鲜橘子；
+值 2 代表腐烂的橘子
+*/
 func orangesRotting(grid [][]int) int {
-	cnts, cost, m, n, queue := 0, 0, len(grid), len(grid[0]), [][]int{}
-	for i, row := range grid {
-		for j, v := range row {
-			if v > 0 {
-				cnts++
-				if v == 2 {
-					queue = append(queue, []int{i, j})
-				}
+	good := 0         // 好橘子的数量
+	bad := [][2]int{} // 坏橘子的位置
+
+	// 遍历所有橘子，计算 好橘子的数量 和 坏橘子的位置
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			switch grid[i][j] {
+			case 1: //如果值是1说明是新鲜橘子，就给计数加一
+				good++
+			case 2: //如果是2说明是腐烂的橘子，加入到数组中
+				bad = append(bad, [2]int{i, j})
 			}
 		}
 	}
-	for len(queue) > 0 {
-		l := len(queue)
-		cnts -= l
-		for i := 0; i < l; i++ {
-			point := queue[0]
-			queue = queue[1:]
-			for _, d := range [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} {
-				nx, ny := point[0]+d[0], point[1]+d[1]
-				if nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1 {
-					grid[nx][ny] = 2
-					queue = append(queue, []int{nx, ny})
-				}
+
+	cnt := 0 // 记录扩散了多少次，相当于结果集
+
+	// 使用 BFS 模拟橘子腐烂并扩散的过程
+	for len(bad) > 0 {
+		next := [][2]int{} // 记录本次腐烂的橘子
+
+		// 开始扩散（遍历所有坏橘子）
+		for len(bad) > 0 {
+			//当前位置腐烂的橘子
+			cur := bad[0]
+			//剔除腐烂过后去橘子，防止重复遍历
+			bad = bad[1:]
+			i, j := cur[0], cur[1] // 拿到坏橘子的位置
+
+			// 判断坏橘子是否在区域内
+			if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) {
+				//如果不在就跳过
+				continue
+			}
+
+			// 往上下左右四个位置扩散（逻辑相同）
+			if i-1 >= 0 && grid[i-1][j] == 1 {
+				good--
+				grid[i-1][j] = 2
+				next = append(next, [2]int{i - 1, j})
+			}
+			if i+1 < len(grid) && grid[i+1][j] == 1 {
+				good--
+				grid[i+1][j] = 2
+				next = append(next, [2]int{i + 1, j})
+			}
+			if j-1 >= 0 && grid[i][j-1] == 1 {
+				good--
+				grid[i][j-1] = 2
+				next = append(next, [2]int{i, j - 1})
+			}
+			if j+1 < len(grid[0]) && grid[i][j+1] == 1 {
+				good--
+				grid[i][j+1] = 2
+				next = append(next, [2]int{i, j + 1})
 			}
 		}
-		if len(queue) > 0 {
-			cost++
+
+		// 本次腐烂的橘子数量大于0时，扩散次数+1
+		if len(next) > 0 {
+			cnt++
 		}
+
+		// 本次腐烂的橘子即为一下次扩散的开始
+		bad = next
 	}
-	if cnts == 0 {
-		return cost
+	//退出循环之后，判断好橘子数量，如果还大于0，说明不可能全部腐烂
+	// 有橘子扩散不到，返回 -1
+	if good > 0 {
+		return -1
 	}
-	return -1
+
+	return cnt
 }
