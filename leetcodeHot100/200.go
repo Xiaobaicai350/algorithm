cef2854cfb8f31549e1d359c2fa73d7832ae4353
胥天昊 <1499487526@qq.com>
2024-03-10 10:58:38 +0800

    200

--- leetcodeHot100/200.go
+++ leetcodeHot100/200.go
@@ -3,29 +3,30 @@ package leetcodeHot100
 func numIslands(grid [][]byte) int {
 	//结果集
 	res := 0
-	//安全性校验，如果
+	//安全性校验，如果长度为0就直接返回有0个岛屿
 	if len(grid) == 0 {
 		return 0
 	}
-
+	//得到row和col
 	nr, nc := len(grid), len(grid[0])
 
 	var dfs func(grid [][]byte, r, c int)
+	//给dfs函数赋值
 	dfs = func(grid [][]byte, r, c int) {
-		nr := len(grid)
-		nc := len(grid[0])
-
-		if r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0' {
+		//如果访问的越界，或者访问的为0，就跳出去。ps:第一行检验越界，第二行检验是否为0
+		if r < 0 || c < 0 || r >= nr || c >= nc ||
+			grid[r][c] == '0' {
 			return
 		}
-
+		//到这里说明没越界也不为0，为1
 		grid[r][c] = '0'
+		//遍历附近的四个方位是否也为1，有1就修改成0
 		dfs(grid, r-1, c)
 		dfs(grid, r+1, c)
 		dfs(grid, r, c-1)
 		dfs(grid, r, c+1)
 	}
-
+	//遍历整个二维数组
 	for r := 0; r < nr; r++ {
 		for c := 0; c < nc; c++ {
 			if grid[r][c] == '1' {
