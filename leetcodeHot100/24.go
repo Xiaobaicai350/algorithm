2c074398117ca74ed1ea557845cb36dae41ba97c
胥天昊 <1499487526@qq.com>
2024-05-13 12:35:47 +0800

    24

--- leetcodeHot100/24.go
+++ leetcodeHot100/24.go
@@ -1,23 +1,24 @@
 package leetcodeHot100
 
 func swapPairs(head *ListNode) *ListNode {
-	//创建虚拟头节点
-	dummyNode := &ListNode{Val: 0}
+	//创建虚拟头结点，后面就可以一视同仁了
+	dummyNode := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
 	dummyNode.Next = head
-
 	//cur用来遍历链表
 	cur := dummyNode
 	for cur.Next != nil && cur.Next.Next != nil {
-		temp1 := cur.Next
-		temp2 := cur.Next.Next
-		temp3 := cur.Next.Next.Next
+		node1 := cur.Next
+		node2 := cur.Next.Next
+		node3 := cur.Next.Next.Next
 
-		//这里不能用temp来表示cur.Next因为cur.Next已经被修改了，而temp还是原来的地方
-		//这里可能有点难理解，用笔画个图就好了
-		cur.Next = temp2
-		cur.Next.Next = temp1
-		cur.Next.Next.Next = temp3
-		//之后再向后移动两步就可以了
+		//只需要改变三个指针
+		node1.Next = node3
+		node2.Next = node1
+		cur.Next = node2
+		//让cur往后移动两步
 		cur = cur.Next.Next
 	}
 	return dummyNode.Next
