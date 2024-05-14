122b4df540c4580e8cc2989467154474a472bba1
胥天昊 <1499487526@qq.com>
2024-05-14 10:00:06 +0800

    25

--- leetcodeHot100/25.go
+++ leetcodeHot100/25.go
@@ -1,39 +1,43 @@
 package leetcodeHot100
 
-// start 代表将要翻转的第一个节点
-// end	 代表将要翻转的最后一个节点
+/*
+输入：head = [1,2,3,4,5], k = 2
+输出：[2,1,4,3,5]
+*/
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	dummy := &ListNode{
-		Val: 0,
-	}
-	dummy.Next = head
-	start := dummy
-	end := dummy
+	dummyNode := &ListNode{}
+	dummyNode.Next = head
+	// start 代表将要翻转的第一个节点的原始前一个节点
+	// end	 代表将要翻转的最后一个节点
+	start := dummyNode
+	end := dummyNode
 	for true {
-		//先把end往后移动k步，如果不足k步就到达尾部了，就不翻转了
+		//先把end往后移动k位，如果不足k位就不需要移动了，说明要退出循环了
 		for i := 0; i < k && end != nil; i++ {
 			end = end.Next
 		}
+		//从上面循环跳出来需要先判断end是否为nil，如果end为nil，说明没必要翻转了
 		if end == nil {
 			break
 		}
-		//记录startNext和endNext
+		//记录下原始头节点
 		startNext := start.Next
+		//记录下原始尾节点的后一个节点
 		endNext := end.Next
-		//将end的next字段制空，方便进行翻转
 		end.Next = nil
-		start.Next = reverse(start.Next)
+		start.Next = reverse(startNext)
 		startNext.Next = endNext
+		//更新一下start和end，方便下次翻转
 		end = startNext
-		start = end
+		start = startNext
 	}
-	return dummy.Next
+	return dummyNode.Next
 }
 
-// 翻转函数，传入head就会将后面的进行翻转
-// 返回值是翻转后的头节点（之前的末尾）
-func reverse(head *ListNode) *ListNode {
-	cur := head
+// 翻转以node为头的链表
+// 返回值是翻转后的链表头结点
+func reverse(node *ListNode) *ListNode {
+	cur := node
 	var pre *ListNode
 	for cur != nil {
 		next := cur.Next
