4a94de052ce86cdbf66f202d15ace6c53b6fb52d
胥天昊 <1499487526@qq.com>
2024-05-15 09:20:30 +0800

    m

--- leetcodeHot100/148.go
+++ leetcodeHot100/148.go
@@ -1,43 +1,28 @@
 package leetcodeHot100
 
+import "sort"
+
+/*
+排序以head为头的链表,思路是把链表转换成数组，然后把数组进行排序，然后再转换成新链表
+*/
+/*
+输入：head = [4,2,1,3]
+输出：[1,2,3,4]
+*/
 func sortList(head *ListNode) *ListNode {
-	//特殊情况进行处理
-	if head == nil || head.Next == nil {
-		return head
+	var ints []int
+	cur := head
+	for cur != nil {
+		ints = append(ints, cur.Val)
+		cur = cur.Next
 	}
-	//为了找到中间位置，定义快慢指针
-	left := head
-	right := head.Next
-	for right != nil && right.Next != nil {
-		//慢指针走一步
-		left = left.Next
-		//快指针走两步
-		right = right.Next.Next
+	sort.Ints(ints)
+	resHead := &ListNode{}
+	cur = resHead
+	for i := 0; i < len(ints); i++ {
+		node := &ListNode{Val: ints[i]}
+		cur.Next = node
+		cur = cur.Next
 	}
-	//当走到这里的时候，left刚好指向中间位置
-	//记录后半部分的指针
-	mid := left.Next
-	//断开前半部分跟后半部分的连接
-	left.Next = nil
-	//递归，注意返回的是排序后的头节点
-	l := sortList(head)
-	r := sortList(mid)
-	res := &ListNode{Val: 0}
-	ans := res
-	for l != nil && r != nil {
-		if l.Val < r.Val {
-			res.Next = l
-			l = l.Next
-		} else {
-			res.Next = r
-			r = r.Next
-		}
-		res = res.Next
-	}
-	if l == nil {
-		res.Next = r
-	} else {
-		res.Next = l
-	}
-	return ans.Next
+	return resHead.Next
 }
--- sort/bubbleSort.go
+++ sort/bubbleSort.go
@@ -1,6 +1,6 @@
 package sort
 
-func bubbleSort(arr []int) {
+func BubbleSort(arr []int) {
 	length := len(arr)
 	for i := 0; i < length-1; i++ {
 		for j := 0; j < length-1-j; j++ {
