b5b3d40905a520e8207d62d3ee95f8482c63b8c2
胥天昊 <1499487526@qq.com>
2024-05-15 09:31:26 +0800

    23

--- leetcodeHot100/23.go
+++ leetcodeHot100/23.go
@@ -1,26 +1,31 @@
 package leetcodeHot100
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+/*
+输入：lists = [[1,4,5],[1,3,4],[2,6]]
+输出：[1,1,2,3,4,4,5,6]
+解释：链表数组如下：
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+将它们合并到一个有序链表中得到。
+1->1->2->3->4->4->5->6
+*/
+
+/*
+这道题其实就是把链表都合并成一个链表，然后进行排序就可以了
+*/
 func mergeKLists(lists []*ListNode) *ListNode {
-	dummy := &ListNode{Val: 0}
-	length := len(lists)
-	tail := dummy
-	//先把k条链表合成一条链表，之后在进行排序
-	for i := 0; i < length; i++ {
-		for lists[i] != nil {
-			temp := lists[i]
-			lists[i] = lists[i].Next
-			tail.Next = temp
-			tail = tail.Next
+	dummyHead := &ListNode{}
+	cur := dummyHead
+	//合并链表
+	for i := 0; i < len(lists); i++ {
+		cur.Next = lists[i]
+		for cur.Next != nil {
+			cur = cur.Next
 		}
 	}
-	//排序
-	list := sortList(dummy.Next)
-	return list
+	//排序链表
+	return sortList(dummyHead.Next)
 }
