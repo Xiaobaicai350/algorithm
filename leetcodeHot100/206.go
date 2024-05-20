f471de32c2a2fe681cf5b6708f91975ee3d5dda3
胥天昊 <1499487526@qq.com>
2024-05-20 09:53:38 +0800

    206

--- leetcodeHot100/206.go
+++ leetcodeHot100/206.go
@@ -1,15 +1,18 @@
 package leetcodeHot100
 
 func reverseList(head *ListNode) *ListNode {
+	//fast用于遍历链表
 	fast := head
+	//slow是fast后面的一个节点
 	var slow *ListNode
 	for fast != nil {
-		//先保存fast后面那个节点
-		fastNext := fast.Next
+		next := fast.Next
+		//一共需要改变一个指针域就可以了
 		fast.Next = slow
+		//移动fast和slow
 		slow = fast
-		fast = fastNext
+		fast = next
 	}
-	//slow不为空，现在fast已经为空了
+	//现在fast为nil，slow为新链表的头
 	return slow
 }
