aa88ff37d3bddf7d976e86d6e45d543d2e384682
胥天昊 <1499487526@qq.com>
2024-05-22 14:46:02 +0800

    33

--- leetcodeHot100/33.go
+++ leetcodeHot100/33.go
@@ -1,27 +1,24 @@
 package leetcodeHot100
 
-// https://www.bilibili.com/video/BV1Ym4y1X72Q/?spm_id_from=333.337.search-card.all.click&vd_source=2259e5459a8cfd21bcf92bc46bf3beda
 func search(nums []int, target int) int {
-	//这道题就是用两个指针分别指向收尾，进行二分
+	//这道题用的是二分查找
 	left := 0
 	right := len(nums) - 1
-	for left <= right {
-		mid := left + (right-left)/2
-		if nums[mid] == target {
-			return mid
-		} else if nums[left] <= nums[mid] { //如果左边界比中间的小的话
-			//说明左边是没翻转的，也就是说左边是连续递增的
-			if nums[left] <= target && target < nums[mid] { //说明target就在左边范围内，就可以进行二分了
-				right = mid - 1
-			} else { //否则就没在，说明在右边
-				left = mid + 1
+	for left <= right { //当left==right的时候仍然有效
+		middle := left + ((right - left) / 2) //等同于 (left + right) / 2 目的是防止溢出
+		if target == nums[middle] {
+			return middle
+		} else if nums[left] <= nums[middle] { //如果满足这个条件，说明目前的左半边是有序的，可以进行二分查找(注意这里必须是<=,因为是向下取整)
+			if target >= nums[left] && target < nums[middle] { //如果在左边这个范围中
+				right = middle - 1
+			} else { //如果不在左边这个范围
+				left = middle + 1
 			}
-		} else {
-			//说明右边是没翻转的，也就是说右边是连续递增的
-			if nums[mid] < target && target <= nums[right] { //说明target在右边范围内
-				left = mid + 1
-			} else { //否则就在左边
-				right = mid - 1
+		} else { //说明右半边有序
+			if target > nums[middle] && target <= nums[right] { //如果在右边这个范围中
+				left = middle + 1
+			} else { //如果不在右边这个范围
+				right = middle - 1
 			}
 		}
 	}
