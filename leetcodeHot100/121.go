91f88d2699d94caa9664d6537735b937425d47a2
胥天昊 <1499487526@qq.com>
2024-05-22 14:14:29 +0800

    121

--- leetcodeHot100/121.go
+++ leetcodeHot100/121.go
@@ -1,20 +1,13 @@
 package leetcodeHot100
 
 func maxProfit(prices []int) int {
-	if len(prices) < 2 {
-		//如果长度为0或者为1
-		//为1的情况说明是当天买当天卖，利润为0
-		//为0的情况说明是空数组，利润当然也为0
-		return 0
-	}
-	//dp[i]的含义是当第i天卖出去，可以获得的最大利润是dp[i]
+	//dp[i]的含义是，当第i+1天卖出股票时，获得最大的利润是dp[i]
 	dp := make([]int, len(prices))
-	//初始化dp数组
+	//第1天卖出股票，获得最大利润是0
 	dp[0] = 0
-	//初始化买入数据
 	buy := prices[0]
-	for i := 1; i < len(prices); i++ {
-		//计算当天的利润
+	for i := 1; i <= len(prices); i++ {
+		//计算利润
 		profit := prices[i] - buy
 		dp[i] = max(profit, dp[i-1])
 		//更新买入的值
