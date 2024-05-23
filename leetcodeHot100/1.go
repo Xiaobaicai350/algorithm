91a95d98f4eb532dff630c31b6e2f005ff9415ad
胥天昊 <1499487526@qq.com>
2024-05-23 10:30:55 +0800

    1

--- leetcodeHot100/1.go
+++ leetcodeHot100/1.go
@@ -1,7 +1,7 @@
 package leetcodeHot100
 
+// 暴力解法
 func twoSum(nums []int, target int) []int {
-	//暴力解法
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
