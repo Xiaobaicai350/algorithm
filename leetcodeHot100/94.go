a597fdd6f35f6709536d192572b42d6b7965c04e
胥天昊 <1499487526@qq.com>
2024-05-26 10:39:52 +0800

    94

--- leetcodeHot100/94.go
+++ leetcodeHot100/94.go
@@ -1,17 +1,17 @@
 package leetcodeHot100
 
-var resArr []int
-
 func inorderTraversal(root *TreeNode) []int {
-	resArr = make([]int, 0)
-	inorder(root)
+	resArr := make([]int, 0)
+	inorder(root, &resArr)
 	return resArr
 }
-func inorder(node *TreeNode) {
+
+// resArr现在是一个指向切片的指针
+func inorder(node *TreeNode, resArr *[]int) {
 	if node == nil {
 		return
 	}
-	inorder(node.Left)
-	resArr = append(resArr, node.Val)
-	inorder(node.Right)
+	inorder(node.Left, resArr)
+	*resArr = append(*resArr, node.Val)
+	inorder(node.Right, resArr)
 }
