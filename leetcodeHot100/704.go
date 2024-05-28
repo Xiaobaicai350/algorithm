e5602f6fd4af4fa634d2399840c2ed997fa9f05c
胥天昊 <1499487526@qq.com>
2024-05-28 20:44:35 +0800

    704

--- leetcodeHot100/704.go
+++ leetcodeHot100/704.go
@@ -1,1 +1,18 @@
 package leetcodeHot100
+
+func binSearch(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	mid := 0
+	for left <= right {
+		mid = (left + right) >> 1
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
