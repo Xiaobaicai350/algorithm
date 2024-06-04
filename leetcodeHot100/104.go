063315c8be1f9ff3929279d064cbcc23b7589b1d
胥天昊 <1499487526@qq.com>
2024-06-04 15:22:47 +0800

    104

--- leetcodeHot100/104.go
+++ leetcodeHot100/104.go
@@ -1,9 +1,11 @@
 package leetcodeHot100
 
 func maxDepth(root *TreeNode) int {
+	//递归出口
 	if root == nil {
 		return 0
 	} else {
+		//只需要考虑一件事，找到左右子树的最大长度就可以了，然后再加上本层的长度
 		return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
 	}
 }
