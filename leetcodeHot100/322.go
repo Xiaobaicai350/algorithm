dbc3846b513880cfeb7f2333132668f20d1b3cb0
胥天昊 <1499487526@qq.com>
2024-06-05 17:16:24 +0800

    322

--- leetcodeHot100/322.go
+++ leetcodeHot100/322.go
@@ -1,28 +1,49 @@
 package leetcodeHot100
 
-// https://www.bilibili.com/video/BV1xA411g7C2/?spm_id_from=333.337.search-card.all.click&vd_source=2259e5459a8cfd21bcf92bc46bf3beda
+/**
+ * 这道题跟完全平方数很像
+ * 当遍历到coin[j]的时候，只需要加上dp[i-coin[j]],就是可能是dp[i]了，但是要算出一个最小的当dp[i]
+ */
+
+/*
+输入：coins = [1, 2, 5], amount = 11
+输出：3
+解释：11 = 5 + 5 + 1
+dp[0]=0
+dp[1]=1
+dp[2]=1
+dp[3]=dp[3-1]+1=2
+dp[4]=dp[4-2]+1=2
+dp[5]=dp[5-5]+1=1
+dp[6]=dp[6-5]+1=2
+dp[7]=dp[7-5]+1=2
+dp[8]=dp[8-5]+1=3
+dp[9]=dp[9-5]+1=3
+dp[10]=dp[10-5]+1=2
+dp[11]=dp[11-5]+1=3
+*/
 func coinChange(coins []int, amount int) int {
-	//dp数组代表的是：当钱数为i的时候，最少需要的钱数是dp[i]
-	//所以数组的长度就是amount+1了
+	// dp[i]的含义是：当总金额为i的时候。可以凑成总金额的最少硬币数为dp[i]
 	dp := make([]int, amount+1)
-	//金额0的最优解dp[0]=0
-	dp[0] = 0
-	//初始化数组其他值为-1
-	for i := 1; i <= amount; i++ {
-		dp[i] = -1
+	// 用一个比较大的值（但是不要用最大值，会溢出）把他填满，方便后面进行比较
+	for i := range dp {
+		dp[i] = amount + 100
 	}
-	//变量从1循环到amount，依次计算金额1到amount的最优解
+	// 初始化dp数组。
+	dp[0] = 0
 	for i := 1; i <= amount; i++ {
-		//对于金额i，使用j遍历面值coins数组
+		// 遍历面值coins数组
 		for j := 0; j < len(coins); j++ {
-			//所有小于等于i的面值coin是[j],如果金额i-coins[j]有最优解
-			if coins[j] <= i && dp[i-coins[j]] != -1 {
-				//如果当前金额还未计算或者dp[i]比正在计算的最优解大
-				if dp[i] == -1 || dp[i] > dp[i-coins[j]]+1 {
-					dp[i] = dp[i-coins[j]] + 1
-				}
+			if coins[j] <= i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
 			}
 		}
 	}
-	return dp[amount]
+	// 如果dp[amount]比amount还大，说明没有任何一种硬币组合能组成总金额
+	// 所以返回-1
+	if dp[amount] > amount {
+		return -1
+	} else { // 否则就是正常情况
+		return dp[amount]
+	}
 }
